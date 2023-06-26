fb4aa0c5d72751d8e2e77a1f10cbc0a7f587e18d
Joshua Dahl <joshuadahl@nevada.unr.edu>
2023-06-25 21:57:39 -0700

    Transform Alias
    Adds an alias that equates raylib::Transform to raylib::Matrix!

--- include/Matrix.hpp
+++ include/Matrix.hpp
@@ -493,6 +493,8 @@ class Matrix : public ::Matrix {
         m15 = mat.m15;
     }
 };
+
+using Transform = Matrix; // raylib::Transform is also a matrix
 }  // namespace raylib
 
 using RMatrix = raylib::Matrix;
