23580ee664185ef3d93897e7df15cbefbaca9edd
Joshua Dahl <joshuadahl@nevada.unr.edu>
2026-01-20 15:36:03 -0800

    Better Shader Set Value
    Allows setting shader value from a reference or a span.
    Also adds versions of the functions which perform automatically find the location of the uniform given its name!

--- include/Shader.hpp
+++ include/Shader.hpp
@@ -2,6 +2,7 @@
 #define RAYLIB_CPP_INCLUDE_SHADER_HPP_
 
 #include <string>
+#include <span>
 
 #include "ShaderUnmanaged.hpp"
 #include "Texture.hpp"
@@ -53,6 +54,183 @@ public:
         if (locs != nullptr) {
             ::UnloadShader(*this);
         }
+    }
+
+    /**
+     * Begin custom shader drawing.
+     */
+    Shader& BeginMode() {
+        ::BeginShaderMode(*this);
+        return *this;
+    }
+
+    /**
+     * End custom shader drawing (use default shader).
+     */
+    Shader& EndMode() {
+        ::EndShaderMode();
+        return *this;
+    }
+
+    /**
+     * Get shader uniform location
+     *
+     * @see GetShaderLocation()
+     */
+    int GetLocation(const std::string& uniformName) const {
+        return ::GetShaderLocation(*this, uniformName.c_str());
+    }
+
+    /**
+     * Get shader attribute location
+     *
+     * @see GetShaderLocationAttrib()
+     */
+    int GetLocationAttrib(const std::string& attribName) const {
+        return ::GetShaderLocationAttrib(*this, attribName.c_str());
+    }
+
+    /**
+     * Set shader uniform value
+     *
+     * @see SetShaderValue()
+     */
+    Shader& SetValue(int uniformLoc, const void* value, int uniformType) {
+        ::SetShaderValue(*this, uniformLoc, value, uniformType);
+        return *this;
+    }
+
+    /**
+     * Set shader uniform value
+     * Automatically finds the location of the uniform given its name
+     *
+     * @see SetShaderValue()
+     */
+    inline Shader& SetValue(std::string uniformName, const void* value, int uniformType) {
+        ::SetShaderValue(*this, GetLocation(uniformName), value, uniformType);
+        return *this;
+    }
+
+    /**
+     * Set shader uniform value
+     *
+     * @see SetShaderValue()
+     */
+    template<typename T>
+    inline Shader& SetValue(int uniformLoc, const T& value, int uniformType) {
+        ::SetShaderValue(*this, uniformLoc, &value, uniformType);
+        return *this;
+    }
+
+    /**
+     * Set shader uniform value
+     * Automatically finds the location of the uniform given its name
+     *
+     * @see SetShaderValue()
+     */
+    template<typename T>
+    inline Shader& SetValue(std::string uniformName, const T& value, int uniformType) {
+        ::SetShaderValue(*this, GetLocation(uniformName), &value, uniformType);
+        return *this;
+    }
+
+    /**
+     * Set shader uniform value vector
+     *
+     * @see SetShaderValueV()
+     */
+    Shader& SetValue(int uniformLoc, const void* value, int uniformType, int count) {
+        ::SetShaderValueV(*this, uniformLoc, value, uniformType, count);
+        return *this;
+    }
+
+     /**
+     * Set shader uniform value vector
+     * Automatically finds the location of the uniform given its name
+     *
+     * @see SetShaderValueV()
+     */
+    inline Shader& SetValue(std::string uniformName, const void* value, int uniformType, int count) {
+        ::SetShaderValueV(*this, GetLocation(uniformName), value, uniformType, count);
+        return *this;
+    }
+
+    /**
+     * Set shader uniform value vector (represented as a std::span)
+     *
+     * @see SetShaderValueV()
+     */
+    template<typename T>
+    inline Shader& SetValue(int uniformLoc, std::span<T> data, int uniformType) {
+        ::SetShaderValueV(*this, uniformLoc, data.value(), uniformType, data.count());
+        return *this;
+    }
+
+    /**
+     * Set shader uniform value vector (represented as a std::span)
+     * Automatically finds the location of the uniform given its name
+     *
+     * @see SetShaderValueV()
+     */
+    template<typename T>
+    inline Shader& SetValue(std::string uniformName, std::span<T> data, int uniformType) {
+        ::SetShaderValueV(*this, GetLocation(uniformName), data.value(), uniformType, data.count());
+        return *this;
+    }
+
+    /**
+     * Set shader uniform value (matrix 4x4)
+     *
+     * @see SetShaderValueMatrix()
+     */
+    Shader& SetValue(int uniformLoc, const ::Matrix& mat) {
+        ::SetShaderValueMatrix(*this, uniformLoc, mat);
+        return *this;
+    }
+
+    /**
+     * Set shader uniform value (matrix 4x4)
+     * Automatically finds the location of the uniform given its name
+     *
+     * @see SetShaderValueMatrix()
+     */
+    inline Shader& SetValue(std::string uniformName, const ::Matrix& mat) {
+        ::SetShaderValueMatrix(*this, GetLocation(uniformName), mat);
+        return *this;
+    }
+
+    /**
+     * Set shader uniform value for texture
+     *
+     * @see SetShaderValueTexture()
+     */
+    Shader& SetValue(int uniformLoc, const ::Texture2D& texture) {
+        ::SetShaderValueTexture(*this, uniformLoc, texture);
+        return *this;
+    }
+
+    /**
+     * Set shader uniform value for texture
+     * Automatically finds the location of the uniform given its name
+     *
+     * @see SetShaderValueTexture()
+     */
+    inline Shader& SetValue(std::string uniformName, const ::Texture2D& texture) {
+        ::SetShaderValueTexture(*this, GetLocation(uniformName), texture);
+        return *this;
+    }
+
+    /**
+     * Retrieves whether or not the shader is ready.
+     */
+    bool IsReady() const {
+        return id != 0 && locs != nullptr;
+    }
+
+ protected:
+    void set(const ::Shader& shader) {
+        id = shader.id;
+        locs = shader.locs;
     }
 };
 } // namespace raylib
